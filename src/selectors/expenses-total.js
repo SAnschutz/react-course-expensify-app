a669c81dcda9c13075730950b4ff398189b600d2
Sue <sanschutz@sue-l-anschutzs-macbook.local>
2018-09-23 11:42:41 -0800

    Add expensify styles

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,6 +1,6 @@
 
 export default (expenses) => {
-    return expenses
-        .map((expense) => expense.amount)
-        .reduce((sum, value) => sum + value, 0);
+  return expenses
+    .map((expense) => expense.amount)
+    .reduce((sum, value) => sum + value, 0);
 };
